173c3d181673f22c5ba38d79d5bdf051df81a0ef
Daniel <mbox2u@hotmail.com>
2019-08-11 19:02:06 +0900

    4

--- js/script.js
+++ js/script.js
@@ -3,7 +3,28 @@ function test() {
     alert('test');
 }
 
+$(document).ready(function(){
+    // init number input
+    var numberInputHtml = '';
+    for(var i=1; i<=36; ++i) {
+        if(1 == i % 5) {
+            numberInputHtml += '<div class="row">';
+        }
+
+        numberInputHtml += '<div class="col">' + 
+                '<div><button type="button" class="btn btn-primary btn-lg" onclick="addNumber(' + i + ')">' + i + '</button></div>' + 
+                '<div id="number-rate-1">0.0%</div>' + 
+            '</div>';
+
+        if(0 == i % 5) {
+            numberInputHtml += '</div>';
+        }
+    }
+
+    $('#number-input').html(numberInputHtml);
+});
+
 function addNumber(number) {
-    $('#historyTextArea').val(
-        $('#historyTextArea').val() + number + ',');
+    $('#history-text-area').val(
+        $('#history-text-area').val() + number + ',');
 }
