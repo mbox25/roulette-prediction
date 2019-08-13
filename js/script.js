b079f1793cc9d39e50e7cba9bd08a7de5c55f781
mbox25 <mbox2u@hotmail.com>
2019-08-13 20:44:18 +0900

    12

--- js/script.js
+++ js/script.js
@@ -3,17 +3,25 @@ $(document).ready(function(){
     reset();   
 });
 
+var lastNumberList = [];
+var lastNumberCountMap = {};
+
 var blackMap = {
     2:0, 4:0, 6:0, 8:0, 10:0, 11:0, 13:0, 15:0, 17:0, 
     20:0, 22:0, 24:0, 26:0, 28:0, 29:0, 31:0, 33:0, 35:0
 };
 
 function reset() {
+    lastNumberList = [];
+    lastNumberCountMap = {};
+
     $('#history-text-area').val('');
     $('#number-input').html('');
-    setNumberPrediction();
-    setOddEvenPrediction();
-    setBlackRedPrediction();
+
+    var newRate = setNumberPrediction();
+    setOddEvenPrediction(newRate);
+    setBlackRedPrediction(newRate);
+    setNumber1to36Prediction(newRate);
 
     var numberInputHtml = '';
     for(var i=1; i<=36; ++i) {
@@ -50,126 +58,180 @@ function reset() {
 }
 
 function undo() {
+    if(0 == lastNumberList.length) {
+        return;
+    }
 
-    // $('#history-text-area').val($('#history-text-area').val().substr());
+    lastNumberCountMap[lastNumberList[lastNumberList.length - 1]] = -1;
+    lastNumberList = lastNumberList.splice(0, lastNumberList.length - 1);
+    var historyStr = '';
+    for(var i=0; i<lastNumberList.length; ++i) {
+        var num = lastNumberList[i];
+        if(37 == num) {
+            historyStr += '00,';
+        }
+        else {
+            historyStr += num + ',';
+        }
+    }
+    $('#history-text-area').val(historyStr);
 
-    // setNumberPrediction();
-    // setOddEvenPrediction();
-    // setBlackRedPrediction();
+    var newRate = setNumberPrediction();
+    setOddEvenPrediction(newRate);
+    setBlackRedPrediction(newRate);
+    setNumber1to36Prediction(newRate);
 }
 
 function addNumber(number) {
-    if(37 == number) {
-        number = '00';  
+    //vibrate();
+
+    lastNumberList.push(number);
+    lastNumberCountMap[number] = 25;
+
+    var historyStr = '';
+    for(var i=0; i<lastNumberList.length; ++i) {
+        var num = lastNumberList[i];
+        if(37 == num) {
+            historyStr += '00,';
+        }
+        else {
+            historyStr += num + ',';
+        }
     }
-    $('#history-text-area').val($('#history-text-area').val() + number + ' ');
-    vibrate();
+    $('#history-text-area').val(historyStr);
     
-    setNumberPrediction();
-    setOddEvenPrediction();
-    setBlackRedPrediction();
+    var newRate = setNumberPrediction();
+    setOddEvenPrediction(newRate);
+    setBlackRedPrediction(newRate);
+    setNumber1to36Prediction(newRate);
 }
 
 navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 function vibrate() {
     if (navigator.vibrate) {
-        navigator.vibrate(100);
-    }
-    else {
-        alert("진동을 지원하지 않는 기종 입니다.");
+        navigator.vibrate(50);
     }
 }
 
 function setNumberPrediction() {
-    var historyData = $('#history-text-area').val();
-    var numberList = historyData.split(' ');
-    var numberCountMap = {};
-    for(var i=0; i<numberList.length -1; ++i) {
-        var number = numberList[i];
-        if('00' == number) {
-            number = 37;  
-        }
-
-        if(number in numberCountMap) {
-            ++numberCountMap[number];
-        }
-        else {
-            numberCountMap[number] = 1;
+    var count = 0;
+    for(var key in lastNumberCountMap) {
+        --lastNumberCountMap[key];
+        if(0 < lastNumberCountMap[key]) {
+            ++count;
         }
     }
 
-    var stdRate = 100 / 38;
-    var remainRate = 0;
-    for(numberKey in numberCountMap) {
-        remainRate += stdRate - (stdRate * (20 - numberCountMap[numberKey]) / 20);
-    }
-
-    var addRate = remainRate / 38;
-    for(var i=0; i<=37; ++i) {
-        var newRate = stdRate + addRate;
-        if(i in numberCountMap) {
-            newRate = (stdRate * (20 - numberCountMap[numberKey]) / 20) + addRate;
+    var newRate = 100 / (38 - count);
+    for(var i=0; i<38; ++i) {
+        var rate = newRate;
+        if(0 < lastNumberCountMap[i]) {
+            rate = 0;
         }
 
-        $('#number-rate-' + i).text(newRate.toFixed(2) + '%');
+        $('#number-rate-' + i).text(rate.toFixed(2) + '%');
     }
+
+    return newRate;
 }
 
-function setOddEvenPrediction() {
-    var historyData = $('#history-text-area').val();
-    var numberList = historyData.split(' ');
-    var oddCount = 0;
-    var evenCount = 0;
-    for(var i=0; i<numberList.length -1; ++i) {
-        var number = numberList[i];
-        if(0 == number || '00' == number) {
+function setOddEvenPrediction(newRate) {
+    var oddRate = 0;
+    var evenRate = 0;
+    for(var i=1; i<=36; ++i) {
+        var rate = newRate;
+        if(0 < lastNumberCountMap[i]) {
             continue;
         }
 
-        if(0 == number % 2) {
-            ++evenCount;
-        }
+        if(0 == i % 2) {
+            evenRate += newRate;
+        } 
         else {
-            ++oddCount;
+            oddRate += newRate;
         }
     }
-
-    var stdRate = 18 / 38 * 100;
-    var evenRate = stdRate + (oddCount - evenCount) * 3.5;
-    var oddRate = stdRate + (evenCount - oddCount) * 3.5;
 
     $('#odd-rate').text(oddRate.toFixed(1) + '%');
     $('#even-rate').text(evenRate.toFixed(1) + '%');
 }
 
-function setBlackRedPrediction() {
+function setBlackRedPrediction(newRate) {
     // var redMap = {
     //     1:0, 3:0, 5:0, 7:0, 9:0, 12:0, 14:0, 16:0, 18:0, 
     //     19:0, 21:0, 23:0, 25:0, 27:0, 30:0, 32:0, 34:0, 36:0
     // }
 
-    var historyData = $('#history-text-area').val();
-    var numberList = historyData.split(' ');
-    var blackCount = 0;
-    var redCount = 0;
-    for(var i=0; i<numberList.length -1; ++i) {
-        var number = numberList[i];
-        if(0 == number || '00' == number) {
+    var blackRate = 0;
+    var redRate = 0;
+    for(var i=1; i<=36; ++i) {
+        var rate = newRate;
+        if(0 < lastNumberCountMap[i]) {
             continue;
         }
 
-        if(number in blackMap) {
-            ++blackCount;
-        }
+        if(i in blackMap) {
+            blackRate += newRate;
+        } 
         else {
-            ++redCount;
+            redRate += newRate;
         }
     }
-
-    var stdRate = 18 / 38 * 100;
-    var blackRate = stdRate + (redCount - blackCount) * 5;
-    var redRate = stdRate + (blackCount - redCount) * 5;
 
     $('#black-rate').text(blackRate.toFixed(1) + '%');
     $('#red-rate').text(redRate.toFixed(1) + '%');
 }
+
+function setNumber1to36Prediction(newRate) {
+    var rate = 0;
+    for(var i=1; i<=18; ++i) {
+        if(0 < lastNumberCountMap[i]) {
+            continue;
+        }
+
+        rate += newRate;
+    }
+    $('#number-1to18-rate').text(rate.toFixed(1) + '%');
+
+    rate = 0;
+    for(var i=19; i<=36; ++i) {
+        if(0 < lastNumberCountMap[i]) {
+            continue;
+        }
+
+        rate += newRate;
+    }
+    $('#number-19to36-rate').text(rate.toFixed(1) + '%');
+
+    rate = 0;
+    for(var i=1; i<=12; ++i) {
+        if(0 < lastNumberCountMap[i]) {
+            continue;
+        }
+
+        rate += newRate;
+    }
+    $('#number-1to12-rate').text(rate.toFixed(1) + '%');
+
+    rate = 0;
+    for(var i=13; i<=24; ++i) {
+        if(0 < lastNumberCountMap[i]) {
+            continue;
+        }
+
+        rate += newRate;
+    }
+    $('#number-13to24-rate').text(rate.toFixed(1) + '%');
+
+    rate = 0;
+    for(var i=25; i<=36; ++i) {
+        if(0 < lastNumberCountMap[i]) {
+            continue;
+        }
+
+        rate += newRate;
+    }
+    $('#number-25to36-rate').text(rate.toFixed(1) + '%');
+}
+
+
